51c611daa714abbf1b98a2a3966caf683fa66bf9
Shishant-Sharma <shishantsharma84@gmail.com>
2026-06-18 17:42:30 +0530

    Finalize task manager backend

--- server.js
+++ server.js
@@ -7,7 +7,12 @@ require("dotenv").config();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI)
